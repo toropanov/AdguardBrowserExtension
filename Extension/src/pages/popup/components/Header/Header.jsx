14eda9808f22cf41ac867b94fd38b49b700dd36a
Anton <tanotify@gmail.com>
2022-11-01 20:11:35 +0300

    Remove Sync button

--- Extension/src/pages/popup/components/Header/Header.jsx
+++ Extension/src/pages/popup/components/Header/Header.jsx
@@ -1,32 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { observer } from 'mobx-react';
-import cn from 'classnames';
 
-import { messenger } from '../../../services/messenger';
 import { Icon } from '../../../common/components/ui/Icon';
-import { addMinDurationTime } from '../../../../common/common-script';
-import { MIN_FILTERS_UPDATE_DISPLAY_DURATION } from '../../../common/constants';
 
 import './header.pcss';
-import { reactTranslator } from '../../../../common/translators/reactTranslator';
 
 export const Header = observer(() => {
-    const [filtersUpdating, setFiltersUpdating] = useState(false);
-
-    const updateFiltersWithMinDuration = addMinDurationTime(
-        messenger.updateFilters.bind(messenger),
-        MIN_FILTERS_UPDATE_DISPLAY_DURATION,
-    );
-
-    const refUpdatingBtn = useRef(null);
-
-    const handleUpdateFiltersClick = async () => {
-        refUpdatingBtn.current.blur();
-        setFiltersUpdating(true);
-        await updateFiltersWithMinDuration();
-        setFiltersUpdating(false);
-    };
-
     return (
         <div className="popup-header">
             <div className="popup-header__logo">
@@ -34,25 +13,6 @@ export const Header = observer(() => {
                     id="#logo"
                     classname="icon--logo"
                 />
-            </div>
-            <div className="popup-header__buttons">
-                <button
-                    className={cn(
-                        'button',
-                        'popup-header__button',
-                        { 'updating-filters': filtersUpdating },
-                    )}
-                    ref={refUpdatingBtn}
-                    disabled={filtersUpdating}
-                    type="button"
-                    onClick={handleUpdateFiltersClick}
-                    title={reactTranslator.getMessage('popup_header_update_filters')}
-                >
-                    <Icon
-                        id="#update-filters"
-                        classname="icon--update-filters"
-                    />
-                </button>
             </div>
         </div>
     );
